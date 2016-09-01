2d0e149285545380750e3742ff888422bcb675b3
J. Cliff Dyer <cdyer@edx.org>
2016-09-01 19:07:43 +0000

    fixup: docstrings.

--- openedx/core/djangoapps/content/course_structures/apps.py
+++ openedx/core/djangoapps/content/course_structures/apps.py
@@ -6,9 +6,14 @@ from django.apps import AppConfig
 
 class CourseStructuresConfig(AppConfig):
     """
-    Custom AppConfig.  Registers Signals
+    Custom AppConfig for openedx.core.djangoapps.content.course_structures
     """
     name = u'openedx.core.djangoapps.content.course_structures'
 
     def ready(self):
+        """
+        Define tasks to perform at app loading time:
+
+        * Connect signal handlers
+        """
         from . import signals  # pylint: disable=unused-variable
